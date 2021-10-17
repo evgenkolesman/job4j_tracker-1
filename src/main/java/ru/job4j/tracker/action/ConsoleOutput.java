ec05d7d074274f70335940ab6dde0a51cd43f211
evgenkolesman <glavstroi_e@mail.ru>
2021-10-17 15:46:35 +0300

    for Docker

--- src/main/java/ru/job4j/tracker/action/ConsoleOutput.java
+++ src/main/java/ru/job4j/tracker/action/ConsoleOutput.java
@@ -2,13 +2,13 @@ package ru.job4j.tracker.action;
 
 import ru.job4j.tracker.trackerformockito.output.Output;
 
-public class ConsoleOutput implements Output {
-    @Override
-    public void writeConsole(String string) {
-        if (string.isEmpty()) {
-            System.out.println("Result is null");
-        } else {
-            System.out.println(string);
-        }
-    }
-}
+//public class ConsoleOutput implements Output {
+//    @Override
+//    public void writeConsole(String string) {
+//        if (string.isEmpty()) {
+//            System.out.println("Result is null");
+//        } else {
+//            System.out.println(string);
+//        }
+//    }
+//}
